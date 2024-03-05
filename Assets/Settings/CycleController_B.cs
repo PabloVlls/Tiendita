3e3a59b793bf4bbf49ba415c0d03e2ef29a43ea2
Carepineso <82180506+Carepineso@users.noreply.github.com>
2024-03-05 11:14:32 -0500

    Cyclos Base

--- Assets/Settings/CycleController_B.cs
+++ Assets/Settings/CycleController_B.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class CycleController_B : MonoBehaviour
 {
     public int dayIndicator;
+
     public int secondsCount;
+    public int modChangeDays;
+
+    public bool itsDay = true;
+    public int modDnN;
+
     public bool timeTicking = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +25,7 @@ public class CycleController_B : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Estamos en el dia:" + dayIndicator);
-        /*if(secondsCount % 10 = 0)
-        {
-            dayIndicator ++;
-        }*/
+        
     }
 
     IEnumerator SecondTick()
@@ -32,10 +35,25 @@ public class CycleController_B : MonoBehaviour
             yield return new WaitUntil(() => timeTicking); 
             yield return new WaitForSeconds(1f);
             secondsCount ++; 
+            DaysCounter();
         }
     }
     
+    public void DaysCounter()
+    {
+        modChangeDays = secondsCount % 10;
+        modDnN = secondsCount % 5;
+        if(modChangeDays == 0)
+        {
+            dayIndicator ++;
+            Debug.Log("Estamos en el dia:" + dayIndicator);
+        }
+        if(modDnN == 0)
+        {
+            itsDay = !itsDay;
+        }
 
+    }
     
 
     
